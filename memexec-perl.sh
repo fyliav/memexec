6b16ee708a15a86314d675278b3b83ebacc86336
Root THC <root@proton.thc.org>
2026-04-11 14:34:22 +0100

    MFD_EXEC: try memfd(16), then 0 for old kernels

--- memexec-perl.sh
+++ memexec-perl.sh
@@ -7,6 +7,6 @@
 # 4314 - mipsel n64
 # 4354 - mipsel o32
 # $^F=255; will disable perl's default O_CLOEXEC.
-memexec(){ LANG="${LANG:-C}" exec perl '-e$^F=255;for(319,279,385,4314,4354){($f=syscall$_,$",16)>0&&last};open($o,">&=".$f);print$o(<STDIN>);exec{"/proc/$$/fd/$f"}"'"${NAME:-/usr/bin/python3}"'",@ARGV;exit 255' -- "$@";}
+memexec(){ LANG="${LANG:-C}" exec perl '-e$^F=255;for(319,279,385,4314,4354){($f=syscall$_,$",16)>0&&last;($f=syscall$_,$",0)>0&&last};open($o,">&=".$f);print$o(<STDIN>);exec{"/proc/$$/fd/$f"}"'"${NAME:-/usr/bin/python3}"'",@ARGV;exit 255' -- "$@";}
 # Example: cat /usr/bin/id | memexec -u
 
